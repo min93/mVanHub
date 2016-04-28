dfe499eba3f88a41f9f998a6dbb156f216abaff2
ECT <min93@live.co.uk>
2016-04-28 14:06:10 +0700

    Before Design

--- app/src/main/java/kmutnb/rattapon/chatupon/chaturaphon/vanhub/EditData.java
+++ app/src/main/java/kmutnb/rattapon/chatupon/chaturaphon/vanhub/EditData.java
@@ -3,17 +3,12 @@ package kmutnb.rattapon.chatupon.chaturaphon.vanhub;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class SignInActivity extends AppCompatActivity {
+public class EditData extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
-    }
-    public void clickLogin(View view){
-    startActivity(new Intent(SignInActivity.this,EditData.class));
-
+        setContentView(R.layout.activity_edit_data);
     }
 }
